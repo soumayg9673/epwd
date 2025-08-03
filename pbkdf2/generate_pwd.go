aea34489e18c85c39c82881d1dd02886f688ba92
soumayg9673 <pmrs9673@gmail.com>
2025-08-03 14:14:21 +0530

    generate encrypted password using usser password and salt

--- pbkdf2/generate_pwd.go
+++ pbkdf2/generate_pwd.go
@@ -27,11 +27,34 @@ func GeneratePasswordWithAutoSalt(password []byte) ([]byte, []byte, error) {
 		config.Hash = sha256.New
 	}
 
-	// Hash password
+	// Encrypt password
 	dk, err := pbkdf2.Key(config.Hash, string(password), []byte(salt), config.Iter, config.KeyLen)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	return dk, salt, nil
+}
+
+func GeneratePasswordWithSalt(password, salt []byte) ([]byte, error) {
+
+	// Validate iteration count
+	if config.Iter == 0 {
+		config.Iter = 600000
+	} else if config.Iter < 0 {
+		return nil, ErrPwdIter
+	}
+
+	// Set SHA256 default when Hash is nil/empty.
+	if config.Hash == nil {
+		config.Hash = sha256.New
+	}
+
+	// Encrypt password
+	dk, err := pbkdf2.Key(config.Hash, string(password), salt, config.Iter, config.KeyLen)
+	if err != nil {
+		return nil, err
+	}
+
+	return dk, nil
 }
